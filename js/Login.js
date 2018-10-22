31c1a0202957fb54be5673f14eab5edfa97f3757
AlonsoIndex18 <alonso_avalo97@hotmail.com>
2018-10-22 18:49:43 -0500

    18:49 22/10/2018

--- js/Login.js
+++ js/Login.js
@@ -9,4 +9,28 @@ function emptyField(){
   }
 }
 
-//
+function login(){
+	var usuario = document.getElementById("username").value;
+	var contraseña = document.getElementById("password").value;
+
+	firebase.auth().signInWithEmailAndPassword(usuario, contraseña).catch(function(error) {
+  	// Handle Errors here.
+  	var errorCode = error.code;
+  	var errorMessage = error.message;
+  	// ...
+		}
+	);
+
+
+}
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+  } else {
+    // No user is signed in.
+  }
+});
+
+
