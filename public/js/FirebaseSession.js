24c41efe4fa7a99cd860bfe8af37ef13ced9be45
AlonsoIndex18 <alonso_avalo97@hotmail.com>
2018-10-24 22:01:19 -0500

    24/10/2018 22:01

---
+++ public/js/FirebaseSession.js
@@ -0,0 +1,13 @@
+(function(){
+// Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyBJfI9p91158CuvOmxPjhNQwuwxfT4enZg",
+    authDomain: "prograweb-fb7c1.firebaseapp.com",
+    databaseURL: "https://prograweb-fb7c1.firebaseio.com",
+    projectId: "prograweb-fb7c1",
+    storageBucket: "prograweb-fb7c1.appspot.com",
+    messagingSenderId: "698758184402"
+  };
+  firebase.initializeApp(config);	
+
+}());
--- public/js/Login.js
+++ public/js/Login.js
@@ -9,29 +9,59 @@ function emptyField(){
   }
 }
 
-function login(){
-  window.alert("h");/*
-	var usuario = document.getElementById("username").value;
-	var contraseña = document.getElementById("password").value;
-
-	firebase.auth().signInWithEmailAndPassword(usuario, contraseña).catch(function(error) {
-  	// Handle Errors here.
-  	var errorCode = error.code;
-  	var errorMessage = error.message;
-  	// ...
-		}*/
-	}
-
-
-
-
-
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    // User is signed in.
+    var user firebase.auth().currentUser;
+    if (user !=null) {
+
+
+    }
   } else {
     // No user is signed in.
   }
-});
+}
+
+  function login(){
+    var email = document.getElementById("username").value;
+    var contra = document.getElementById("password").value;
+    
+    if (!firebase.auth().isSignInWithEmailLink(emailLink)) {
+      alert("Verifique el enlace enviado a "+email);
+      firebase.auth().signInWithEmailLink(email)
+      .catch(function(error) {
+      // Some error occurred, you can inspect the code: error.code
+      // Common errors could be invalid email and invalid or expired OTPs.
+      });
+    }else {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode === 'auth/wrong-password') {
+      alert('Wrong password.');
+    } else {
+      alert(errorMessage);
+    }
+  console.log(error);
+  });
+    }
 
 
+
+   }
+
+
+
+
+
+function recuperarContra(){
+  var auth = firebase.auth();
+  var emailAddress = "user@example.com";
+
+  auth.sendPasswordResetEmail(emailAddress).then(function() {
+  // Email sent.
+  }).catch(function(error) {
+  // An error happened.
+  });
+}
