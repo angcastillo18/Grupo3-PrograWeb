86ebf3b36ba09d9b50e958083651d6200bacb77f
Alonso Avalo <alonso_avalo97@hotmail.com>
2018-10-28 13:47:59 -0500

    13:47 28/10/2018

--- public/js/Registro.js
+++ public/js/Registro.js
@@ -5,15 +5,21 @@
 function registro(){
 
   if (verificarDatos()===true) {
+    var mail = document.getElementById("mail").value;
+    var psw = document.getElementById("psw").value;
     firebase.auth().createUserWithEmailAndPassword(mail,psw)
     .catch(function(error) {
     // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       if (errorCode == 'auth/weak-password') {
-        alert('weakpass');
-      } else if('auth/email-already-in-use'){
-        alert('passinuse');
+        alert('La contraseña ingresada no tiene los requisitos mínimos de seguridad');
+      } else if(errorCode =='auth/email-already-in-use'){
+        alert('El correo '+mail+" ya está en uso");
+      } else if(errorCode == 'auth/invalid-email'){
+        alert('Debe ingresar un correo válido');
+      }else {
+        alert("Correo creado satisfactoriamente")
       }
       console.log(error);
     });
@@ -31,7 +37,7 @@ function verificarDatos() {
   var phone = document.getElementById("phone").value;
   var regex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/;
 
-alert(user+name+mail+psw+phone);
+alert(mail+psw+phone);
 
  if ( (user == null || user.length == 0 || /^\s+$/.test(user)) ) {
    // Si no se cumple la condicion...
@@ -43,7 +49,7 @@ alert(user+name+mail+psw+phone);
    alert('Es obligatorio llenar el campo de nombre y apellidos ');
    return false;
  }
- else if ( regex.test(mail)){
+ else if ( (regex.test(mail))||mail.length == 0 || mail==null){
    // Si no se cumple la condicion...
    alert('Debe ingresar un correo válido');
    return false;
@@ -53,7 +59,7 @@ alert(user+name+mail+psw+phone);
    alert('La contraseña debe tener al menos 6 caracteres');
    return false;
  }
- else if ( (!(/^\d{9}$/.test(phone))) || phone=="") {
+ else if ( (!(/^\d{9}$/.test(phone)))&&phone.length>0) {
    // Si no se cumple la condicion...
    alert('Debe ingresar un número de teléfono válido, si no está seguro, deje la casilla en blanco');
    return false;
